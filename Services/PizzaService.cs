7a9aa22fd54e913be00220b405b5c64c3c21b5d3
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2021-12-27 20:20:51 -0300

    Add project files.

---
+++ Services/PizzaService.cs
@@ -0,0 +1,51 @@
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Services;
+
+public static class PizzaService
+{
+    static List<Pizza> Pizzas { get;}
+    static int nextId=3;
+    static PizzaService()
+    {
+        Pizzas = new List<Pizza>
+        {
+            new Pizza {Id = 1, Name = "Classic Italian", IsGlutenFree = false},
+            new Pizza {Id = 2, Name = "Veggie", IsGlutenFree = true}
+        };
+    }
+
+    public static List<Pizza> GetAll() => Pizzas;
+    
+    public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+
+        if (pizza is null)
+            return;
+        
+        Pizzas.Remove(pizza);
+    }
+
+    public static void Update(Pizza pizza)
+    {
+        var pizzaIndex = Get(pizza.Id);
+
+        if (pizzaIndex is null)
+            return;
+
+        var index = Pizzas.IndexOf(pizzaIndex);
+        if (index == -1)
+            return;
+
+        Pizzas[index] = pizza;
+    }
+}
